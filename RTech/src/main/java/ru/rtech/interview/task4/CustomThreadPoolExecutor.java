1df50e0f6c71b67b5bd403cf4b894d86988f8941
VirKato-dev <dascat79@mail.ru>
2024-05-23 09:33:09 +0300

    rtech

--- RTech/src/main/java/ru/rtech/interview/task4/CustomThreadPoolExecutor.java
+++ RTech/src/main/java/ru/rtech/interview/task4/CustomThreadPoolExecutor.java
@@ -1,9 +1,21 @@
 package ru.rtech.interview.task4;
 
-import java.util.concurrent.*;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
-public class CustomThreadPoolExecutor /*extends ThreadPoolExecutor*/ {
-//    public CustomThreadPoolExecutor(int maximumPoolSize) {
-//        super(0, maximumPoolSize, 0, 0, );
-//    }
+public class CustomThreadPoolExecutor extends ThreadPoolExecutor {
+    public CustomThreadPoolExecutor(int maximumPoolSize) {
+        super(0, maximumPoolSize, 0, TimeUnit.MICROSECONDS, new LinkedBlockingQueue<>());
+    }
+
+    @Override
+    public void execute(Runnable command) {
+        if (command.getClass().isAnnotationPresent(Repeat.class)) {
+            int repeat = command.getClass().getAnnotation(Repeat.class).value();
+            for (int i = 0; i < repeat; i++) {
+                super.execute(command);
+            }
+        }
+    }
 }
--- RTech/src/main/java/ru/rtech/interview/task4/Solution.java
+++ RTech/src/main/java/ru/rtech/interview/task4/Solution.java
@@ -2,8 +2,7 @@ package ru.rtech.interview.task4;
 
 public class Solution {
     public static void main(String[] strings) {
-//        CustomThreadPoolExecutor customThreadPoolExecutor =
-//                new CustomThreadPoolExecutor(10);
-//        customThreadPoolExecutor.execute(new MyRunnable());
+        CustomThreadPoolExecutor customThreadPoolExecutor = new CustomThreadPoolExecutor(10);
+        customThreadPoolExecutor.execute(new MyRunnable());
     }
 }
